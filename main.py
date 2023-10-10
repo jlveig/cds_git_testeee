2d446a2e3deabe2ab567b0e7c99d836dcae48da4
jlveig <jlveigantes@gmail.com>
2023-10-10 15:03:21 -0300

    adicionado operacoes matematicas

---
+++ main.py
@@ -0,0 +1,13 @@
+def gather_data():
+    n1 = int(input("primeiro valor: "))
+    n2 = int(input("segundo valor: "))
+
+    return n1, n2
+
+def main():
+    print(2*2)
+    return None
+
+if __name__ == "__main__":
+    main()
+    
